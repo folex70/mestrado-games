57d5e3bcfb8b7cf282caa945dacaae0c7aa62344
folex70 <folex70@gmail.com>
2016-03-07 22:04:51 -0300

    Controle da tela de seleção de temas implantado

---
+++ mestrado-games/Assets/Scripts/gameThemes.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gameThemes : MonoBehaviour {
+
+	public Button btnPlay;
+	public Text textNomeTema;
+
+	public GameObject infoTemas;
+	public Text textInfoTema;
+	public GameObject medalhaBronze;
+	public GameObject medalhaPrata;
+	public GameObject medalhaOuro;
+
+	public string[] nomeTema;
+
+	private int idTema;
+	private int numeroQuestoes =20;
+	private int acertos = 0;
+	private int notaFinal;
+
+	// Use this for initialization
+	void Start () {
+		idTema = 0;
+		textNomeTema.text = nomeTema [idTema];
+		textInfoTema.text = "Voce Acertou X de X questoes";
+		infoTemas.SetActive (false);
+		medalhaBronze.SetActive (false);
+		medalhaPrata.SetActive (false);
+		medalhaOuro.SetActive (false);
+		btnPlay.interactable = false;
+
+	}
+
+	public void selectTheme(int i)
+	{
+		idTema = i;
+		textNomeTema.text = nomeTema [i];
+		infoTemas.SetActive (true);
+		btnPlay.interactable = true;
+		textInfoTema.text = "Voce Acertou "+acertos+" de "+numeroQuestoes;
+	}
+
+	public void play()
+	{
+		Application.LoadLevel ("quiz-tema"+idTema);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ mestrado-games/Assets/Scripts/infoTheme.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class infoTheme : MonoBehaviour {
+
+
+	public int idTema;
+
+	public GameObject medalhaBronze;
+	public GameObject medalhaPrata;
+	public GameObject medalhaOuro;
+
+	private int notaFinal;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		medalhaBronze.SetActive (false);
+		medalhaPrata.SetActive (false);
+		medalhaOuro.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
